f6a61c401425f1fa7611e596ceba4fc981ba98c2
Ryder Belserion <contact@ryderbelserion.com>
2025-06-17 20:58:30 -0400

    Bump to 1.21.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 rootProject.description = "The lobby plugin for play.corecraft.me!"
-rootProject.version = "1.0.0"
+rootProject.version = "1.1.0"
 rootProject.group = "me.corecraft"
 
 allprojects {
